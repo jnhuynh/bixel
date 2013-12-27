7b1bd8b096004f4318e8568f0e1c1fc7aa826205
John Huynh <jn@huynh.io>
2013-12-26 23:42:38 -0500

    Added CRUD API for players.

---
+++ app/controllers/api/v1/players_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::PlayersController < ApplicationController
+  def index
+    render(:json => Player.all)
+  end
+
+  def show
+    render(:json => Player.find(params[:id]))
+  end
+
+  def create
+    player = Player.new(player_params)
+
+    player.save
+    render(:json => player)
+  end
+
+  def update
+    player = Player.find(params[:id])
+    player.update_attributes(player_params)
+
+    render(:json => player)
+  end
+
+  def destroy
+    player = Player.find(params[:id])
+    player.destroy
+
+    render(:json => player, :status => 204)
+  end
+
+  private
+
+    def players_params
+      params.require(:player).permit(:name)
+    end
+end
