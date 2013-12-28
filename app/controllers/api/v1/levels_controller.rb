735c4cc620a9f226f55f297e40b352aea54d7daf
John Huynh <jn@huynh.io>
2013-12-27 19:08:40 -0500

    Added remaining level CRUD api endpoints.

--- app/controllers/api/v1/levels_controller.rb
+++ app/controllers/api/v1/levels_controller.rb
@@ -2,4 +2,35 @@ class Api::V1::LevelsController < ApplicationController
   def index
     render(:json => Level.all)
   end
+
+  def show
+    render(:json => Level.find(params[:id]))
+  end
+
+  def create
+    level = Level.new(level_params)
+
+    level.save
+    render(:json => level)
+  end
+
+  def update
+    level = Level.find(params[:id])
+    level.update_attributes(level_params)
+
+    render(:json => level)
+  end
+
+  def destroy
+    level = Level.find(params[:id])
+    level.destroy
+
+    render(:json => level, :status => 204)
+  end
+
+  private
+
+    def level_params
+      params.require(:level).permit(:name, :width, :height)
+    end
 end
