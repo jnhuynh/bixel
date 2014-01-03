947dc1eea7bccf79cf3d4cd745677db2bfa9d1f8
John Huynh <john@huynh.io>
2014-01-02 21:41:16 -0500

    Removed duplicated players in level serializer.

--- app/serializers/level_serializer.rb
+++ app/serializers/level_serializer.rb
@@ -1,6 +1,6 @@
 class LevelSerializer < ActiveModel::Serializer
   embed(:ids)
 
-  attributes(:id, :width, :height, :name, :updated_at, :players)
+  attributes(:id, :width, :height, :name, :updated_at)
   has_many(:players)
 end
