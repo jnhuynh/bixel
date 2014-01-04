7042b8127a1aa0c4111a4e42101c0bf6b36bcd04
John Huynh <john@huynh.io>
2014-01-04 16:20:45 -0500

    Moved require websockets js to application.js

--- config/application.js
+++ config/application.js
@@ -4,6 +4,7 @@
 //= require_self
 //= require adapter
 //= require router
+//= require websocket_rails/main
 //= require_tree ../app
 //= require_tree ../vendor/assets/javascripts
 //= require_tree ./initializers
