db43661c10516aa40a1d188166b08ca818e74bf7
John Huynh <john@huynh.io>
2014-01-15 20:21:39 -0500

    Swapped imp for "night elf" based off barbarian.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 plains = Level.create(:name => 'Plains', :width => 640,  :height => 512)
 
-imp       = Player.create(:name => 'Imp', :direction => 'down', :x => 0, :y => 0)
+night_elf = Player.create(:name => 'Night Elf', :direction => 'down', :x => 0, :y => 0)
 barbarian = Player.create(:name => 'Barbarian', :direction => 'down', :x => 0, :y => 0 )
 
-SpriteSheet.create(:src => '/images/imp-walk.png', :state => 0,
+SpriteSheet.create(:src => '/images/night-elf-walk.png', :state => 0,
   :number_states => 4, :tile_width => 32, :tile_height => 48,
-  :player_id => imp.id)
+  :player_id => night_elf.id)
 SpriteSheet.create(:src => '/images/barbarian-walk.png', :state => 0,
   :number_states => 4, :tile_width => 32, :tile_height => 48,
   :player_id => barbarian.id)
