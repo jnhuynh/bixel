afd6a14a91de1170d7ffc0db1aa8fed9a9f89a48
John Huynh <john@huynh.io>
2014-01-15 19:37:03 -0500

    Updated barbarian spritesheet.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ imp       = Player.create(:name => 'Imp', :direction => 'down', :x => 0, :y => 0
 barbarian = Player.create(:name => 'Barbarian', :direction => 'down', :x => 0, :y => 0 )
 
 SpriteSheet.create(:src => '/images/imp-walk.png', :state => 0,
-  :number_states => 4, :tile_width => 48, :tile_height => 64,
+  :number_states => 4, :tile_width => 32, :tile_height => 48,
   :player_id => imp.id)
 SpriteSheet.create(:src => '/images/barbarian-walk.png', :state => 0,
-  :number_states => 4, :tile_width => 48, :tile_height => 64,
+  :number_states => 4, :tile_width => 32, :tile_height => 48,
   :player_id => barbarian.id)
