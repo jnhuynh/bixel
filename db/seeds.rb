cd336485fd0e0cc3038d136fe45181cf3f401faa
John Huynh <john@huynh.io>
2014-03-05 19:46:28 -0500

    Added DB seeds and ran db:setup.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Area.create(:name => "Plains", :width => 640,  :height => 512)
+
+night_elf = Player.create(:name => "Night Elf", :direction => "down",
+  :x => 0, :y => 0)
+
+barbarian = Player.create(:name => "Barbarian", :direction => "down",
+  :x => 0, :y => 0 )
+
+Spritesheet.create(:name => "Night Elf",
+  :src => "/images/night-elf-walk.png",
+  :width => 128, :height => 192,
+  :frame_width => 32, :frame_height => 48,
+  :frame_columns => 4, :frame_rows => 4,
+  :current_frame_column => 0, :current_frame_row => 0,
+  :player_id => night_elf.id)
+
+Spritesheet.create(:name => "Barbarian",
+  :src => "/images/barbarian-walk.png",
+  :width => 128, :height => 192,
+  :frame_width => 32, :frame_height => 48,
+  :frame_columns => 4, :frame_rows => 4,
+  :current_frame_column => 0, :current_frame_row => 0,
+  :player_id => barbarian.id)
