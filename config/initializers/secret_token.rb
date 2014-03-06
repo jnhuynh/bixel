bde02508f378e25bd96558e16e45f36ba01d1fd5
John Huynh <john@huynh.io>
2014-03-05 19:15:23 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bixel::Application.config.secret_key_base = '9c63392f135931b81054c52a333441f04cf4560674ea35673b6040e9d0ce8296aa2d641922a897b30417b9c076de67ab84b0df474a3207bf9ba537071384cf37'
