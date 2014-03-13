d974163f2f0c2073018e8f2da577ff94c34f9856
John Huynh <john@huynh.io>
2014-03-13 19:05:56 -0400

    Added player#update API endpoint.

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -5,7 +5,17 @@ class Api::V1::PlayersController < ApplicationController
   end
 
   def show
-    @player = Player.where(:id => params[:id])
+    @player = Player.where(:id => params[:id]).first
+    render(:json => @player)
+  end
+
+  def update
+    @player = Player.where(:id => params[:id]).first
+    @area   = Area.where(:id => params[:player][:area]).first
+
+    @player.area = @area
+    @player.save
+
     render(:json => @player)
   end
 end
